292ef8effe45470624593f2be48843b948780f32
Øivind Pettersen <oivindpettersen94@gmail.com>
2023-01-17 17:54:40 +0100

    new changes

---
+++ src/views/FolderPage.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator';
+import Vue from 'vue'
+
+interface Cake {
+  name: string;
+  image: string;
+  description: string;
+  ingredients: string[];
+  price: number;
+}
+
+@Component
+export default class CakeCard extends Vue {
+  @Prop() cake!: Cake; 
+}
+</script>
+
+<template>
+  <ion-card>
+    <ion-card-header>
+      <ion-card-title>{{cake.name}}</ion-card-title>
+    </ion-card-header>
+    <ion-img :src="cake.image"></ion-img>
+    <ion-card-content>
+      <p>{{cake.description}}</p>
+      <p>Ingredients: {{cake.ingredients.join(', ')}}</p>
+      <p>Price: {{cake.price}}</p>
+    </ion-card-content>
+  </ion-card>
+</template>
+
